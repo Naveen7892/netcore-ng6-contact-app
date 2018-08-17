3e30e70894d3d2be16dc4fc95cbe652761567c77
Naveen Kumar <metanaveen@outlook.com>
2018-08-17 12:45:45 +0530

    Created MSSQL DB using migration InitialCreate and packages added

---
+++ Models/ContactAppContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace contact_app.Models
+{
+    public class ContactAppContext: DbContext {
+        public ContactAppContext(DbContextOptions < ContactAppContext > options): base(options) {
+
+        }
+        public DbSet < Contact > Contact {
+            get;
+            set;
+        }
+    }
+}
